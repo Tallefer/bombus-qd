e732036f18f2847d33f576e04132bf3bf7e1e1b6
kotov.alexej <kotov.alexej@fdf5eb46-60f3-11de-8abc-d1c337b90d21>
2009-06-24 19:43:13 +0000

    *проба2

---
+++ src/Account/AccountSelect.java
@@ -0,0 +1,358 @@
+/*
+ * AccountSelect.java
+ *
+ * Created on 19.03.2005, 23:26
+ * Copyright (c) 2005-2008, Eugene Stahov (evgs), http://bombus-im.org
+ * Copyright (c) 2009, Alexej Kotov (aqent), http://bombusmod-qd.wen.ru
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * You can also redistribute and/or modify this program under the
+ * terms of the Psi License, specified in the accompanied COPYING
+ * file, as published by the Psi Project; either dated January 1st,
+ * 2005, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+package Account;
+import Client.*;
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import com.alsutton.jabber.datablocks.Presence;
+import locale.SR;
+import midlet.BombusQD;
+import ui.*;
+import java.io.*;
+import java.util.*;
+//#ifndef MENU_LISTENER
+//# import javax.microedition.lcdui.CommandListener;
+//# import javax.microedition.lcdui.Command;
+//#else
+import Menu.MenuListener;
+import Menu.Command;
+import Menu.MyMenu;
+//#endif
+import ui.MainBar;
+import io.NvStorage;
+import ui.controls.AlertBox;
+import util.Strconv;
+//#ifdef GRAPHICS_MENU        
+//# import ui.GMenu;
+//#endif
+import Colors.ColorTheme; 
+
+public class AccountSelect 
+    extends VirtualList 
+    implements
+//#ifndef MENU_LISTENER
+//#         CommandListener
+//#else
+        MenuListener
+//#endif
+    {
+
+    public Vector accountList;
+    int activeAccount;
+    boolean enableQuit;
+    
+    Command cmdLogin=new Command(SR.MS_SELLOGIN, Command.OK,1);
+
+//#ifdef GRAPHICS_MENU   
+//#     Command cmdRegister=new Command(SR.MS_REGISTERING,Command.ITEM,11);   
+//#         Command cmdServ1_reg=new Command("Jabber.ru", Command.SCREEN,18); 
+//#         Command cmdServ2_reg=new Command("Silper.cz", Command.SCREEN,19);
+//#         Command cmdServ3_reg=new Command("Jabbus.org", Command.SCREEN,20); 
+//#         Command cmdServ4_reg=new Command("Mytlt.ru", Command.SCREEN,21); 
+//#         Command cmdServ5_reg=new Command("Jabbim.com", Command.SCREEN,22);  
+//#         Command cmdServ6_reg=new Command("Gajim.org", Command.SCREEN,23);
+//# 
+//#     Command cmdAdd=new Command(SR.MS_NEW_ACCOUNT, Command.SCREEN,12);    
+//#         Command cmdJabber=new Command("Jabber", Command.SCREEN,13);    
+//#         Command cmdYaru=new Command("Yandex.ru", Command.SCREEN,14);    
+//#         Command cmdGTalk_SSL=new Command("Gtalk SSL", Command.SCREEN,15);    
+//#         Command cmdGTalk_HTTPS=new Command("Gtalk HTTPS", Command.SCREEN,16);            
+//#         Command cmdLj=new Command("LiveJournal", Command.SCREEN,17);   
+//#         Command cmdQip=new Command("QIP", Command.SCREEN,18);
+//#endif    
+   
+    Command cmdEdit=new Command(SR.MS_EDIT,Command.ITEM,3);
+    Command cmdDel=new Command(SR.MS_DELETE,Command.ITEM,4);
+    Command cmdConfig=new Command(SR.MS_OPTIONS,Command.ITEM,5);
+    Command cmdCancel=new Command(SR.MS_BACK,Command.BACK,99);
+    Command cmdQuit=new Command(SR.MS_APP_QUIT,Command.SCREEN,10);
+    
+    Command cmdRemoveAcc=new Command(SR.MS_REMOVE_ACCOUNT,Command.ITEM,90);
+    Command cmdChangePass=new Command(SR.MS_CHANGE_PASSWORD,Command.ITEM,91);    
+    
+    private Config cf;
+    int status;
+    /** Creates a new instance of AccountPicker */
+    public AccountSelect(Display display, Displayable pView, boolean enableQuit,int status) {
+        super();
+        this.enableQuit=enableQuit;
+        this.display=display;
+        this.status=status;
+        
+        cf=Config.getInstance();
+        
+        String str = "";
+        switch(status){
+            case 0: str = "(online)"; break;
+            case 1: str = "(chat)"; break;
+            case 2: str = "(away)"; break;
+            case 3: str = "(xa)"; break;
+            case 4: str = "(dnd)"; break;
+            case 5: str = "(offline)"; break;
+            case 6: str = ""; break;
+        }
+        setMainBarItem(new MainBar(status==-1?SR.MS_ACCOUNTS:SR.MS_CONNECT_TO + str));
+
+        accountList=null;
+        accountList=new Vector();
+        Account a;
+        
+        int index=0;
+        activeAccount=cf.accountIndex;
+        do {
+            a=Account.createFromStorage(index);
+            if (a!=null) {
+                a.setActive(activeAccount==index);
+                accountList.addElement(a);
+                index++;
+             }
+       } while (a!=null);
+        
+        if (!accountList.isEmpty()) {
+            moveCursorTo(activeAccount);
+        }else{
+           ColorTheme.loadSkin("/themes/default.txt", 1);
+        }
+        commandState();
+        setCommandListener(this);
+        attachDisplay(display);
+        this.parentView=pView;
+    }
+    
+    
+    public void commandState(){
+//#ifdef MENU_LISTENER
+        menuCommands.removeAllElements();
+        cmdfirstList.removeAllElements();
+        cmdsecondList.removeAllElements();
+        cmdThirdList.removeAllElements();
+//#endif
+        if (!accountList.isEmpty()) {
+            addCommand(cmdLogin);  cmdLogin.setImg(0x50);
+            addCommand(cmdEdit); cmdEdit.setImg(0x40);
+            addCommand(cmdDel); cmdDel.setImg(0x41);
+        }
+        
+        if (StaticData.getInstance().roster.isLoggedIn()){
+          if(cf.difficulty_level==2) {  
+            addCommand(cmdRemoveAcc); cmdRemoveAcc.setImg(0x51);
+            addCommand(cmdChangePass); cmdChangePass.setImg(0x52);
+          }
+        }
+//#ifdef GRAPHICS_MENU
+//#         addCommand(cmdRegister); cmdRegister.setImg(0x42);
+//#            addInCommand(2,cmdServ1_reg); cmdServ1_reg.setImg(0x86);
+//#            addInCommand(2,cmdServ2_reg); cmdServ2_reg.setImg(0x85);
+//#            addInCommand(2,cmdServ3_reg); cmdServ3_reg.setImg(0x86);
+//#            addInCommand(2,cmdServ4_reg); cmdServ4_reg.setImg(0x86);
+//#            addInCommand(2,cmdServ5_reg); cmdServ5_reg.setImg(0x85);           
+//#            addInCommand(2,cmdServ6_reg); cmdServ6_reg.setImg(0x72);
+//# 
+//#         addCommand(cmdAdd); cmdAdd.setImg(0x42);
+//#            addInCommand(1,cmdJabber); cmdJabber.setImg(0x90);
+//#            addInCommand(1,cmdYaru); cmdYaru.setImg(0x91);
+//#            addInCommand(1,cmdGTalk_SSL); cmdGTalk_SSL.setImg(0x92);
+//#            addInCommand(1,cmdGTalk_HTTPS); cmdGTalk_HTTPS.setImg(0x92);           
+//#            addInCommand(1,cmdLj); cmdLj.setImg(0x93);
+//#            addInCommand(1,cmdQip); cmdQip.setImg(0x94);
+//#endif          
+          
+        if (accountList.isEmpty()==true) {
+            addCommand(cmdQuit);  cmdQuit.setImg(0x33);
+            addCommand(cmdConfig); cmdConfig.setImg(0x03);
+        }        
+        
+//#ifndef GRAPHICS_MENU   
+    if (activeAccount>=0 && !enableQuit)
+     addCommand(cmdCancel);
+//#endif            
+    }
+
+    public VirtualElement getItemRef(int Index) { return (VirtualElement)accountList.elementAt(Index); }
+    protected int getItemCount() { return accountList.size();  }
+
+    public void commandAction(Command c, Displayable d){
+        if (c==cmdQuit) {
+            destroyView();
+            BombusQD.getInstance().notifyDestroyed();
+            return;
+        }
+         if (c==cmdCancel) {
+             destroyView();
+         }
+//#ifdef GRAPHICS_MENU  
+//#         if(c==cmdServ1_reg){
+//#           new AccountForm(display, this, this, null,1,true,"jabber.ru");  
+//#         }
+//#         if(c==cmdServ2_reg){
+//#           new AccountForm(display, this, this, null,1,true,"silper.cz");            
+//#         }        
+//#         if(c==cmdServ3_reg){
+//#           new AccountForm(display, this, this, null,1,true,"jabbus.org");            
+//#         }
+//#         if(c==cmdServ4_reg){
+//#           new AccountForm(display, this, this, null,1,true,"mytlt.ru");            
+//#         }
+//#         if(c==cmdServ5_reg){
+//#           new AccountForm(display, this, this, null,1,true,"jabbim.com");            
+//#         }
+//#         if(c==cmdServ6_reg){
+//#           new AccountForm(display, this, this, null,1,true,"gajim.org");   
+//#         }
+//# 
+//#         if (c==cmdJabber) {
+//#           new AccountForm(display, this, this, null,1,false,null);  
+//#         }
+//#         if (c==cmdYaru) {
+//#           new AccountForm(display, this, this, null,2,false,null);
+//#         }
+//#         
+//#         if (c==cmdGTalk_SSL) {
+//#           new AccountForm(display, this, this, null,3,false,null);
+//#         }        
+//#         if (c==cmdGTalk_HTTPS) {
+//#           new AccountForm(display, this, this, null,6,false,null);
+//#         }
+//#         
+//#         
+//#         if (c==cmdLj) {
+//#           new AccountForm(display, this, this, null,4,false,null);
+//#         }
+//#         if (c==cmdQip) {
+//#           new AccountForm(display, this, this, null,5,false,null);
+//#         }          
+//#else   
+        if (c==cmdAdd) {
+            new AccountForm(display, this, this, null);
+        }
+//#endif           
+        if (c==cmdConfig) new ConfigForm(display, this);
+        if (c==cmdLogin) switchAccount(true);
+        if (c==cmdEdit) new AccountForm(display, this, this, (Account)getFocusedObject(),-1,false,null);
+        if(c==cmdChangePass){
+          Object cursor_acc=getFocusedObject();
+          Object  active_acc =  accountList.elementAt(activeAccount);
+          if(active_acc.equals(cursor_acc)){
+            Account acc =  (Account)getFocusedObject(); 
+            new CommandForm(display,parentView,2,"Form",acc,this);
+          }
+
+        }
+        if(c==cmdRemoveAcc){
+          Object cursor_acc=getFocusedObject();
+          Object  active_acc =  accountList.elementAt(activeAccount);
+          if(active_acc.equals(cursor_acc)){
+           Account acc =  (Account)getFocusedObject();
+           new CommandForm(display,parentView,1,"Form",acc,accountList);
+          }
+        }        
+        if (c==cmdDel) {
+            if (cursor==cf.accountIndex && StaticData.getInstance().roster.isLoggedIn()) return;
+            new AlertBox(SR.MS_DELETE, getFocusedObject().toString(), display, this) {
+                public void yes() {
+                    delAccount();
+                }
+                public void no() { }
+            };
+        }
+    }
+
+    public void touchRightPressed(){
+        destroyView();
+    }
+
+    public void destroyView(){
+       display.setCurrent(StaticData.getInstance().roster);
+    }
+
+    private void delAccount(){
+        if (accountList.size()==1) 
+            cf.accountIndex=-1;
+        else if (cf.accountIndex>cursor) cf.accountIndex--;
+
+        cf.saveToStorage();
+
+        accountList.removeElement(getFocusedObject());
+        rmsUpdate();
+        moveCursorHome();
+        commandState();
+        redraw();
+    }
+    
+    private void switchAccount(boolean login){
+        cf.accountIndex=cursor;
+        cf.saveToStorage();
+        Account.loadAccount(login, cursor, status);
+        destroyView();
+    }
+    
+    public void eventOk(){
+          if (getItemCount()>0) {
+             switchAccount(true);
+          } 
+    }
+    
+    public void rmsUpdate(){
+        DataOutputStream outputStream=NvStorage.CreateDataOutputStream();
+        for (int i=0;i<accountList.size();i++) 
+            ((Account)accountList.elementAt(i)).saveToDataOutputStream(outputStream);
+        NvStorage.writeFileRecord(outputStream, "accnt_db", 0, true); //Account.storage
+    }
+
+//#ifdef MENU_LISTENER
+
+    
+//#ifdef GRAPHICS_MENU        
+//#     public int showGraphicsMenu() {
+//#         commandState();
+//#         new GMenu(display, parentView, this, null, menuCommands, cmdfirstList, cmdsecondList, cmdThirdList);
+//#         GMenuConfig.getInstance().itemGrMenu = GMenu.ACCOUNT_SELECT_MENU;
+//#         return GMenu.ACCOUNT_SELECT_MENU;
+//#     }
+//#else
+    public void showMenu() {
+        commandState();
+        new MyMenu(display, parentView, this, SR.MS_DISCO, null, menuCommands);
+   }   
+//#endif
+
+//#endif
+    
+    protected void keyRepeated(int keyCode) {
+        super.keyRepeated(keyCode);
+        if (kHold==keyCode) return;
+        kHold=keyCode;
+        if (keyCode==KEY_NUM6) {
+            cf.fullscreen=!cf.fullscreen;
+            cf.saveToStorage();
+            VirtualList.fullscreen=cf.fullscreen;
+            StaticData.getInstance().roster.setFullScreenMode(cf.fullscreen);
+        }
+    }
+}
